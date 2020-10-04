a2e8d4ada33e4f66e4d563e433f5792c92f87711
Michael Fladischer <FladischerMichael@fladi.at>
2020-10-04 23:41:42 +0200

    Add Indizes for Activity, CompetenceLevel and Symptom.

--- LZK/search_indexes.py
+++ LZK/search_indexes.py
@@ -15,6 +15,42 @@ class ObjectiveIndex(indexes.SearchIndex, indexes.Indexable):
         return self.get_model().objects.exclude(depth=None)#.filter(public=True)
 
 
+class SymptomIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    #subjects = indexes.CharField(model_attr='subject')
+    #pub_date = indexes.DateTimeField(model_attr='pub_date')
+
+    def get_model(self):
+        return models.Symptom
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
+
+
+class CompetenceLevelIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    #subjects = indexes.CharField(model_attr='subject')
+    #pub_date = indexes.DateTimeField(model_attr='pub_date')
+
+    def get_model(self):
+        return models.CompetenceLevel
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
+
+
+class ActivityIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    #subjects = indexes.CharField(model_attr='subject')
+    #pub_date = indexes.DateTimeField(model_attr='pub_date')
+
+    def get_model(self):
+        return models.Activity
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
+
+
 class SubjectIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     #subjects = indexes.CharField(model_attr='subject')
