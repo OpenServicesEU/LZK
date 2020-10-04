2a7d9b66121852082bc897ea4b17e19c19e4651e
Michael Fladischer <FladischerMichael@fladi.at>
2020-10-04 23:14:49 +0200

    Use WEBP for slides.

--- LZK/imagegenerators.py
+++ LZK/imagegenerators.py
@@ -4,8 +4,8 @@ from imagekit.processors import ResizeToFill
 
 class Slide(ImageSpec):
     processors = [ResizeToFill(1110, 624)]
-    format = "JPEG"
-    options = {"quality": 60}
+    format = "WEBP"
+    options = {"quality": 70}
 
 
 register.generator("LZK:slide", Slide)
